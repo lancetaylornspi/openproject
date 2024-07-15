7c66b0ee8c3c19451a50499a2348de94b37fcebe
Henriette Darge <h.darge@openproject.com>
2024-07-15 08:26:31 +0200

    Document Submenu architecture as pattern

--- lookbook/previews/open_project/common/submenu_component_preview.rb
+++ lookbook/previews/open_project/common/submenu_component_preview.rb
@@ -3,6 +3,7 @@ module OpenProject
     # @logical_path OpenProject/Common
     class SubmenuComponentPreview < Lookbook::Preview
       # @label Default
+      # @display min_height 450px
       def default
         render_with_template(template: "open_project/common/submenu_preview/playground",
                              locals: { sidebar_menu_items: menu_items, searchable: false, create_btn_options: nil })
@@ -17,11 +18,40 @@ module OpenProject
       end
 
       # @label With create Button
-      # `create_btn_options: { href: "/#", text: "User"}`
+      # `create_btn_options: { href: "/#", module_key: "user"}`
       def with_create_button
         render_with_template(template: "open_project/common/submenu_preview/playground",
-                             locals: { sidebar_menu_items: menu_items, searchable: true,
+                             locals: { sidebar_menu_items: menu_items,
+                                       searchable: true,
                                        create_btn_options: { href: "/#", module_key: "user" } })
+      end
+
+      # @label With leading icon
+      # `create_btn_options: { href: "/#", module_key: "user"}`
+      # TODO: show the items
+      def with_leading_icons
+        render_with_template(template: "open_project/common/submenu_preview/playground",
+                             locals: { sidebar_menu_items: [
+                                         OpenProject::Menu::MenuGroup.new(
+                                           header: "Favourite",
+                                           children: [
+                                             OpenProject::Menu::MenuItem.new(title: "Watched", href: "", selected: false,
+                                                                             icon: :eye),
+                                             OpenProject::Menu::MenuItem.new(title: "Assigned", href: "", selected: true,
+                                                                             icon: :"op-person-assigned")
+                                           ]
+                                         )
+                                       ],
+                                       searchable: true,
+                                       create_btn_options: { href: "/#", module_key: "user" } })
+      end
+
+      def with_favourite_query
+        # TODO
+      end
+
+      def with_enterprise_query
+        # TODO
       end
 
       private
@@ -31,24 +61,23 @@ module OpenProject
           OpenProject::Menu::MenuGroup.new(
             header: nil,
             children: [
-              OpenProject::Menu::MenuItem.new(title: I18n.t("members.menu.all"), href: "", selected: true),
-              OpenProject::Menu::MenuItem.new(title: I18n.t("members.menu.locked"), href: "", selected: false),
-              OpenProject::Menu::MenuItem.new(title: I18n.t("members.menu.invited"), href: "", selected: false)
+              OpenProject::Menu::MenuItem.new(title: "Global query", href: "", selected: true),
+              OpenProject::Menu::MenuItem.new(title: "A second query", href: "", selected: false)
             ]
           ),
           OpenProject::Menu::MenuGroup.new(
-            header: I18n.t("members.menu.project_roles"),
+            header: "Favourite",
             children: [
-              OpenProject::Menu::MenuItem.new(title: "Developer", href: "", selected: false),
-              OpenProject::Menu::MenuItem.new(title: "Manager", href: "", selected: true)
+              OpenProject::Menu::MenuItem.new(title: "Very important version", href: "", selected: false),
+              OpenProject::Menu::MenuItem.new(title: "A baseline view", href: "", selected: true)
             ]
           ),
           OpenProject::Menu::MenuGroup.new(
-            header: I18n.t("members.menu.groups"),
+            header: "Private",
             children: [
-              OpenProject::Menu::MenuItem.new(title: "UX", href: "", selected: false),
-              OpenProject::Menu::MenuItem.new(title: "Customer success", href: "", selected: false),
-              OpenProject::Menu::MenuItem.new(title: "Core dev team", href: "", selected: false)
+              OpenProject::Menu::MenuItem.new(title: "My query", href: "", selected: false),
+              OpenProject::Menu::MenuItem.new(title: "An awesome query", href: "", selected: false),
+              OpenProject::Menu::MenuItem.new(title: "A third query", href: "", selected: false)
             ]
           )
         ]
